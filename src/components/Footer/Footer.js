ff49f6b05142270805c3a44469380be5258059c4
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2023-08-14 21:25:34 -0400

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://linkedin.com/erintomorri/'
+      href='https://www.linkedin.com/in/erin-tomorri/?originalSubdomain=ca'
       className='link footer__link'
     >
       Follow me on Linkedin!
